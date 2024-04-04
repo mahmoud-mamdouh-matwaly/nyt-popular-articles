b76cb088fe1654f94d5d5be5a6a26f4cc2fd9128
mahmoud mamdouh metwally <mahmoud.m.alawady@gmail.com>
2024-04-04 03:15:08 +0200

    test: add spinner

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import { Spinner, SpinnerProps } from '@chakra-ui/react'
 
 export function BaseSpinner(props: SpinnerProps) {
-  return <Spinner thickness='5px' color='blue.400' size='xl' {...props} />
+  return <Spinner data-testid='spinner' thickness='5px' color='blue.400' size='xl' {...props} />
 }
